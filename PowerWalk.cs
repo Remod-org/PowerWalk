90df38ffcb0f34b97d94e77e254a37f36e4de3ce
RFC1920 <desolationoutpostpve@gmail.com>
2023-12-27 18:01:31 -0600

    Add license

--- PowerWalk.cs
+++ PowerWalk.cs
@@ -1,3 +1,23 @@
+#region License (GPL v2)
+/*
+    PowerWalk
+    Copyright (c) 2023 RFC1920 <desolationoutpostpve@gmail.com>
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License v2.0.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+    Optionally you can also view the license at <http://www.gnu.org/licenses/>.
+*/
+#endregion License (GPL v2)
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
